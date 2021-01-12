34c298b27b9fdb4851b0066ac763205c8c083446
AssalaMohsen <asallaamuhsen@gmail.com>
2021-01-12 15:25:14 +0200

    livewire search updated

--- app/Http/Livewire/SearchUsers.php
+++ app/Http/Livewire/SearchUsers.php
@@ -8,11 +8,13 @@ use Livewire\Component;
 class SearchUsers extends Component
 {
     public $search = '';
+    public $isActive = true;
 
     public function render()
     {
+        $user = User::where('username', $this->search)->get();
         return view('livewire.search-users', [
-            'users' => User::where('username', $this->search)->get(),
+            'users' =>$user,
         ]);
     }
 }
--- resources/views/livewire/search-users.blade.php
+++ resources/views/livewire/search-users.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div class="form-group">
-        <input wire:model="search" class="thinborder-gray rounded p-2" type="text" style="outline: none;"
+        <input wire:model="search" class="thinborder-gray rounded p-1" type="text" style="outline: none;"
             placeholder="Search by username" />
 
             <ul>
                 @foreach ($users as $user)
-                    <li><a href="/profiles/{{$user->username}}" class="flex items-center text-sm mt-2"><img src="{{$user->avatar}}"  alt="" class="avatar w-h-40 mr-2"/>{{ $user->name }}</a></li>
+                    <li><a href="/profiles/{{$user->username}}" class="flex items-center text-sm mt-2"><img src="{{$user->avatar}}"  alt="" class="avatar w-h-30 mr-2"/>{{ $user->name }}</a></li>
                 @endforeach
             </ul>
     </div>
